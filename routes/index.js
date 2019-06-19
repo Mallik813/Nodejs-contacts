cceebee40376c9361a1780ac1f38d5ccfc9c26a7
Katakam Mallikarjun <mallik813@gmail.com>
2019-06-20 00:52:47 +0530

    till delete

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+
+const passport = require('passport');
+// Load User model
+const User = require('../models/user');
+const DB = require('../config/keys');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+router.get('/home', (req, res)=>{ 
+  if(req.user){
+    res.render('home', {
+      user : req.user,
+      Login:true
+    })
+  }
+  else{
+    res.render('home', {
+      Login: false
+    })
+  }
+});
+
+
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user,
+    ID: req.user._id
+  })
+);
+router.get('/vie', (req, res)=>{
+  User.find({}, (err, docs)=>{
+    if(err) res.json(err);
+    // else res.send(docs);
+    else res.render('vie', {users: docs});
+  });
+});
+
+
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,188 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+// Load User model
+const User = require('../models/user');
+const DB = require('../config/keys');
+
+const { forwardAuthenticated } = require('../config/auth');
+
+// Login Page
+router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
+
+// Register Page
+router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
+
+// Register
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/users/login');
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
+
+// Login
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+router.post('/task', (req, res)=>{
+    // console.log(req.body);
+    if (req.body.task_id == '')
+        insertRecord(req, res);
+        else
+        updateRecord(req, res);
+  
+});
+function insertRecord(req, res)
+{
+  const {_id, task}  = req.body;
+  let errors = [];
+  console.log(req.body);
+  if (!task) {
+    errors.push({ msg: ' Please enter a task ' });
+  }  
+
+  
+  User.updateOne({ _id:_id }, {$push:{tasks:{task}}}, { new: true }, (err, doc) => {
+    if (!err) { res.redirect('../dashboard?Successfully+Updated'); }
+    else
+      console.log('Error during record update : ' + err);
+    
+});
+}
+function updateRecord(req, res)
+{
+  console.log("byee");
+}
+
+router.post ('/update', (req, res)=>{
+    const {_id, name, email} = req.body;
+    let errors=[];
+    console.log(req.body);
+
+    User.updateOne({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+      if (!err) { res.redirect('../dashboard?Successfully+Updated'); }
+      else
+        console.log('Error during record update : ' + err);
+      
+  });
+});
+
+router.get('/tasks/:user_id/delete/:task_id', (req, res)=>{
+  
+  let errors=[];
+  // console.log(req.params.user_id);
+  // console.log(req.params.task_id);
+
+  User.findOneAndUpdate(
+    {_id: req.params.user_id}, 
+    {$pull: {tasks: {_id: req.params.task_id}}},
+    function(err, data){
+       if(err) return err;
+       else
+       res.redirect('../../../../dashboard?Task+deleted');
+      //  res.send({data:data});
+});
+}); 
+
+router.get('/tasks/:user_id/update/:task_id', (req, res)=>{
+
+  User.findById(req.params.user_id   , (err, doc) => {
+    console.log(doc.tasks);
+//     if (!err) {
+//         res.render("dashboard", {doc});
+//     }
+});
+
+
+//   console.log(req.params.task_id.task);
+  
+//   User.findOneAndUpdate(  
+//     { 
+//         "_id": req.params.user_id,
+//         "tasks._id": req.params.task_id
+//     },
+//     { "$set": { 
+//       "tasks.$.task": "Make it zero"      
+//   } } ,
+//     function(err, data){
+//       if(err) return err;
+//       else
+//       res.redirect('../../../../dashboard?Task');
+//       }      
+// )
+});
+  
+
+
+
+module.exports = router;
